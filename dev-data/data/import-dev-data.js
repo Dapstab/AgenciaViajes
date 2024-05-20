b26b1f2b110b6f4c70d7dfd76f6fe08c96c2f6a0
Dapstab <dspt1996@gmail.com>
2024-05-19 22:56:15 -0500

    corrección de errores

--- dev-data/data/import-dev-data.js
+++ dev-data/data/import-dev-data.js
@@ -18,7 +18,9 @@ mongoose.connect(DB).then((con) => {
 });
 
 // READ JSON FILE
-const tours = JSON.parse(fs.readFileSync(`${__dirname}/tours.json`, "utf-8"));
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/tours-col.json`, "utf-8")
+);
 const users = JSON.parse(fs.readFileSync(`${__dirname}/users.json`, "utf-8"));
 const reviews = JSON.parse(
   fs.readFileSync(`${__dirname}/reviews.json`, "utf-8")
